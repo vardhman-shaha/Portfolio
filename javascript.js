17336774dae8e5e1444704588e017212b45682be
vardhman-shaha <153358944+vardhman-shaha@users.noreply.github.com>
2024-10-19 19:36:40 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function detectMobile() {
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        window.location.href = "android.html"; // Redirect to Android layout page
-    }
-}
-
-window.onload = detectMobile;
